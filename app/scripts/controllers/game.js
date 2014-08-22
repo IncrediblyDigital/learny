69eb713862c1bf173c1d56eee495eee151ecd6d7
Josh Kieschnick <jjkiesch@gmail.com>
2014-08-22 09:15:39 -0500

    Refactored a little bit

--- app/scripts/controllers/game.js
+++ app/scripts/controllers/game.js
@@ -1,53 +1,52 @@
+/*global _:false */
 'use strict';
 
-var _ = _;
-
 angular.module('learnyApp')
-  .controller('GameCtrl', ['$scope', function ($scope) {
-    $scope.gameTitle = 'Alphabet Game';
-    var letters = _.shuffle('abcdefghijklmnopqrstuvwxyz'.split(''));
-    var remainingLetters = letters.slice(0);
+  .controller('GameCtrl', [function () {
+    var self = this;
+    self.gameTitle = 'Alphabet Game';
+    var letters = 'abcdefghijklmnopqrstuvwxyz'.split('');
+    var remainingLetters = _.shuffle(letters.slice(0));
     var answerPool = [];
     var currentLetter = '';
 
     var numRight = 0;
     var numWrong = 0;
     var numAnswered = 0;
-    $scope.message = 'Select the matching lower case letter';
+    self.message = 'Select the matching lower case letter';
+    self.remainingQuestions = remainingLetters.length;
 
     var updateProgress = function() {
-      $scope.progress = Math.floor(((letters.length-remainingLetters.length)/letters.length)*100);
-      $scope.rightAnswers = Math.floor((numRight/numAnswered)*100);
-      $scope.wrongAnswers = Math.floor((numWrong/numAnswered)*100);
-    }
-
-    $scope.remainingQuestions = remainingLetters.length;
+      self.progress = Math.floor(((letters.length-remainingLetters.length)/letters.length)*100);
+      self.rightAnswers = Math.floor((numRight/numAnswered)*100);
+      self.wrongAnswers = Math.floor((numWrong/numAnswered)*100);
+    };
 
     var initLetter = function() {
-      $scope.givenAnswers = [];
+      self.givenAnswers = [];
       currentLetter = remainingLetters.pop();
-      $scope.currentLetter = currentLetter.toUpperCase();
+      self.currentLetter = currentLetter.toUpperCase();
       answerPool = _.without(letters.slice(0), currentLetter);
-      $scope.givenAnswers.push(currentLetter);
-      while ($scope.givenAnswers.length < 4) {
-        $scope.givenAnswers.push(answerPool.pop());
+      self.givenAnswers.push(currentLetter);
+      while (self.givenAnswers.length < 4) {
+        self.givenAnswers.push(answerPool.pop());
       }
-      $scope.remainingQuestions = remainingLetters.length;
-      $scope.givenAnswers = _.shuffle($scope.givenAnswers);
+      self.remainingQuestions = remainingLetters.length;
+      self.givenAnswers = _.shuffle(self.givenAnswers);
     };
 
     initLetter();
     updateProgress();
 
-    $scope.checkAnswer = function(clickedLetter) {
+    self.checkAnswer = function(clickedLetter) {
       if (clickedLetter === currentLetter) {
         numRight++;
-        $scope.message = 'You got it right!';
-        $scope.lastCorrect = true;
+        self.message = 'You got it right!';
+        self.lastCorrect = true;
       } else {
         numWrong++;
-        $scope.message = 'You must not be able to read good...';
-        $scope.lastCorrect = false;
+        self.message = 'You must not be able to read good...';
+        self.lastCorrect = false;
       }
       numAnswered++;
       initLetter();

96a125ca8962a90ac4ed785d50927630511c5679
Angi Burton <angimburton@gmail.com>
2014-09-17 21:11:23 -0500

    Added images, some design

--- Gruntfile.js
+++ Gruntfile.js
@@ -66,10 +66,10 @@ module.exports = function (grunt) {
     // The actual grunt server settings
     connect: {
       options: {
-        port: 9000,
+        port: 8000,
         // Change this to '0.0.0.0' to access the server from outside.
         hostname: 'localhost',
-        livereload: 35729
+        livereload: 35730
       },
       livereload: {
         options: {
@@ -210,7 +210,7 @@ module.exports = function (grunt) {
         src: [
           '<%= yeoman.dist %>/scripts/{,*/}*.js',
           '<%= yeoman.dist %>/styles/{,*/}*.css',
-          '<%= yeoman.dist %>/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}',
+         // '<%= yeoman.dist %>/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}',
           '<%= yeoman.dist %>/styles/fonts/*'
         ]
       }
--- app/scripts/controllers/game.js
+++ app/scripts/controllers/game.js
@@ -9,11 +9,12 @@ angular.module('learnyApp')
     var remainingLetters = _.shuffle(letters.slice(0));
     var answerPool = [];
     var currentLetter = '';
+    
 
     var numRight = 0;
     var numWrong = 0;
     var numAnswered = 0;
-    self.message = 'Select the matching lower case letter';
+    self.message = 'Choose the correct lower case letter';
     self.remainingQuestions = remainingLetters.length;
 
     var updateProgress = function() {
@@ -26,13 +27,14 @@ angular.module('learnyApp')
       self.givenAnswers = [];
       currentLetter = remainingLetters.pop();
       self.currentLetter = currentLetter.toUpperCase();
-      answerPool = _.without(letters.slice(0), currentLetter);
+      answerPool = _.without(_.shuffle(letters.slice(0)), currentLetter);
       self.givenAnswers.push(currentLetter);
       while (self.givenAnswers.length < 4) {
         self.givenAnswers.push(answerPool.pop());
       }
       self.remainingQuestions = remainingLetters.length;
       self.givenAnswers = _.shuffle(self.givenAnswers);
+    
     };
 
     initLetter();
@@ -41,15 +43,17 @@ angular.module('learnyApp')
     self.checkAnswer = function(clickedLetter) {
       if (clickedLetter === currentLetter) {
         numRight++;
-        self.message = 'You got it right!';
+
+        self.message = 'RIGHT! That was the letter ' + currentLetter.toUpperCase() + '!';
         self.lastCorrect = true;
       } else {
         numWrong++;
-        self.message = 'You must not be able to read good...';
+        self.message = 'WRONG! That was NOT the letter ' + currentLetter.toUpperCase() + '!';
         self.lastCorrect = false;
       }
       numAnswered++;
       initLetter();
       updateProgress();
     };
+    
   }]);
